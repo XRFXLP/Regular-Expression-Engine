20ab7a4592e3cfa5ac8b60aa9de22e4eba8e44ff
Ajay Mishra <Ajaym6405@gmail.com>
2021-01-24 10:18:25 +0530

    Update main.py

--- idiot-engine/main.py
+++ idiot-engine/main.py
@@ -1,7 +1,7 @@
 from postfX import *
 from utility import match
 
-
+#this won't work :P
 #Should work, I've not tested as I M S PDO OPDOPOEPOEPDOEPODEPODEPDOEPDOE
 def fits(regex, string):
   p = postfix(regex)
